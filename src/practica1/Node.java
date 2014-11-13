ba6b0f18806bb911cbf2a01a9e4086f305edc276
guilnassim <guilhermenassim@gmail.com>
2014-11-13 11:07:06 +0100

    List of nodes

--- src/practica1/Node.java
+++ src/practica1/Node.java
@@ -10,6 +10,80 @@ package practica1;
  *
  * @author User
  */
+class GLinkedList<E>{
+	private Node<E> head = null;//empty list
+	private int size = 0;
+	public void add(E x){ //add at head
+		Node<E> nw = new Node<E>(x);
+		nw.setNext(head);
+		head = nw;
+		size++;
+	}
+
+	public boolean contains(E x){
+		Node<E> k = head;
+		boolean found = false;
+		while(k != null && !found){
+			E kk = k.data();
+			if(kk.equals(x)) found = true;
+			else k = k.next();
+		}
+		return found;
+	}
+
+	public void remove(E x){
+		Node<E> k = head; Node<E> bk = head;
+		boolean found = false;
+		while(k != null && !found){
+			if(k.data().equals(x)) found = true;
+			else{ bk = k; k = k.next();}
+		}
+		if(found)
+			if(k == head)
+				head = k.next();
+			else
+			  bk.setNext(k.next());
+	}
+
+	public int length(){
+		return size;
+	}
+	public void display(){
+		Node<E> k = head;
+		System.out.print('[');
+		while(k != null){
+			if(k.next != null)
+			   System.out.print(k.data()+", ");
+			else
+			   System.out.print(k.data());
+			k = k.next();
+		}
+		System.out.println(']');
+	}
+	public Iterator<E> iterator(){
+		return new GIterator<E>(head, size);
+	}
+	private static class GIterator<E> implements Iterator<E>{
+		private Node <E> head;
+		private int size;
+		private int index = 0;
+		GIterator(Node<E> h, int s){
+			head = h; size = s;
+		}
+		public boolean hasNext(){
+			return index < size;
+		}
+		public E next(){
+			if(index == size) throw new NoSuchElementException();
+			E item = head.data();
+			head = head.next(); index++;
+			return item;
+		}
+		public void remove(){}
+	}
+}
+
+
 public class Node<E>{
 	E data;
 	Node<E> next;
